5c255a46d4bc6263dc781df6bdeb2e062dd9ad68
Haszprus <haszprus@gmail.com>
2015-10-16 21:10:11 +0200

    jshint fix

--- client/app/scripts/user/isValid.js
+++ client/app/scripts/user/isValid.js
@@ -1,25 +1,23 @@
 /**
  * Calls $setValidity on the input field with true or false based on a bool variable given as the directive's value.
  */
-angular.module('meanformApp').directive('isValid', ['$parse', function ($parse) {
+angular.module('meanformApp').directive('isValid', [function () {
   "use strict";
 
   var directive = {
     restrict: 'A',
     require: ['ngModel'],
-    link: link
+    link: function (scope, elem, attrs, ctrl) {
+      var isValid = scope.$eval(attrs.isValid);
+      var modelController = ctrl[0];
+
+      attrs.$observe('isValid', function () {
+        isValid = scope.$eval(attrs.isValid);
+        modelController.$setValidity('byModel', isValid);
+      });
+    }
   };
 
   return directive;
 
-  function link(scope, elem, attrs, ctrl) {
-    var isValid = scope.$eval(attrs.isValid);
-    var modelController = ctrl[0];
-
-    attrs.$observe('isValid', function()
-    {
-      isValid = scope.$eval(attrs.isValid);
-      modelController.$setValidity('byModel', isValid);
-    });
-  }
 }]);
